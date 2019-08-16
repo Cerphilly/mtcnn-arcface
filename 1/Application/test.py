30eb0da5fe2d1920c240c36e0851a0edb5e23bf4
Cerphilly <37470073+Yoonhee-Gil@users.noreply.github.com>
2019-08-16 10:27:33 +0900

    Update test.py

--- 1/Application/test.py
+++ 1/Application/test.py
@@ -15,7 +15,7 @@ import time
 
 parser = argparse.ArgumentParser(description='face model test')
 parser.add_argument('--image-size', default='112,112', help='')
-parser.add_argument('--model', default='/Users/aham/PycharmProjects/1/Pretrained_model/model-r100-ii', help='path to load model.')
+parser.add_argument('--model', default='/Pretrained_model/model-r100-ii', help='path to load model.')
 parser.add_argument('--ga-model', default='', help='path to load gender age model.')
 parser.add_argument('--gpu', default=0, type=int, help='gpu id')
 parser.add_argument('--det', default=0, type=int, help='mtcnn option, 1 means using R+O, 0 means detect from begining')
